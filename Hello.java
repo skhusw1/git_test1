cb250b33d140318e055568ae52e2777fb975b1f9
Lee, Seungjin <quack337@gmail.com>
2025-03-07 16:13:30 +0900

    message 멤버 변수 추가

--- Hello.java
+++ Hello.java
@@ -1,6 +1,8 @@
 public class Hello {
 
-    public void say() {
-        System.out.println("안녕하세요");
-    }
+  String message = "안녕하세요";
+
+  public void say() {
+      System.out.println(message);
+  }
 }
